42fbd398270cd0c33aefd7a756787db65e3b06fe
kakhi-kotchauri <87081106+kakhi-kotchauri@users.noreply.github.com>
2021-12-31 16:57:09 +0400

    edited calculator buttons

--- src/components/button.js
+++ src/components/button.js
@@ -8,14 +8,14 @@ export const Styledbutton = (props) => {
   const{children, onck, ...rest} = props
 
   const Mybutton = styled.button`
-  color: ${rest.color};
-  background: ${rest.backgroundColor};
-  width: ${rest.width};
-  height: ${rest.height};
-  border-radius: ${rest.radius};
-  margin: ${rest.margin};
-  font-size: ${rest.font};
-  display: ${rest.display};
+  color: ${rest.color || 'red'}  ;
+  background: ${rest.backgroundColor || 'white'};
+  width: ${rest.width || '5vw'};
+  height: ${rest.height || '2vw'};
+  border-radius: ${rest.radius || '0.3vw'};
+  margin: ${rest.margin || '0vw 0vw 0vw 0vw'};
+  font-size: ${rest.font || '1.5vw'};
+  display: ${rest.display || 'inline-block'};
   justify-content: center;
   align-items: center;
   vertical-align: top;
@@ -26,4 +26,29 @@ export const Styledbutton = (props) => {
         <Mybutton onClick={onck}>{children}</Mybutton>
         </div>
     )
+}
+
+
+export const Buttonv1 = (props) => {
+  const{children, onck, ...rest} = props
+
+  const Mybutton = styled.button`
+  color: ${rest.color || 'red'}  ;
+  background: ${rest.backgroundColor || 'white'};
+  width: ${rest.width || '10vw'};
+  height: ${rest.height || '10vw'};
+  border-radius: ${rest.radius || '0.3vw'};
+  margin: ${rest.margin || '1vw 0vw 3vw 3vw'};
+  font-size: ${rest.font || '9vw'};
+  display: ${rest.display || 'inline-flex'};
+  justify-content: center;
+  align-items: center;
+  vertical-align: top;
+`;
+
+  return (
+    <div style={{display:rest.pardisplay, justifyContent: 'center'}}>
+      <Mybutton onClick={onck}>{children}</Mybutton>
+      </div>
+  )
 }
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Styledbutton } from "./button";
 import { useState } from "react";
+import { Buttonv1 } from "./button";
 
 export const Calc = () => {
 
@@ -36,96 +37,53 @@ const math = (type) => {
 
 }
 
-
     return (
         <div style={{marginLeft: '3vw'}}>
-      <Styledbutton 
+      <Buttonv1
       onck = {() => math('0')}
-      display = {'inline-flex'} 
-      pardisplay = {'inline-flex'} 
-      radius = {'0.3vw'} 
-      font = {'9vw'} 
-      margin = {'1vw 0vw 3vw 3vw'} 
-      height = '10vw'
-      width = '10vw' 
-      backgroundColor = {'green'} 
-      color = {'black'}
-      lh = {'0vw'}>
-      0
-      </Styledbutton>
-      <Styledbutton 
+      color = 'black'
+      backgroundColor = 'green'
+      pardisplay = 'inline-flex'>
+      0</Buttonv1>
+
+      <Buttonv1
       onck = {() => math('*')}
-      display = {'inline-flex'} 
-      pardisplay = {'inline-flex'} 
-      radius = {'0.3vw'} 
-      font = {'9vw'} 
-      margin = {'1vw 0vw 3vw 3vw'} 
-      height = {'10vw'} 
-      width = {'10vw'} 
-      backgroundColor = {'red'} 
-      color = {'black'}
-      lh = {'0vw'}>
-      x
-      </Styledbutton>
-      <Styledbutton 
+      color = 'black'
+      backgroundColor = 'red'
+      pardisplay = 'inline-flex'>
+      x</Buttonv1>
+
+      <Buttonv1
       onck = {() => math('/')}
-      display = {'inline-flex'} 
-      pardisplay = {'inline-flex'} 
-      radius = {'0.3vw'} 
-      font = {'9vw'} 
-      margin = {'1vw 0vw 3vw 3vw'} 
-      height = {'10vw'} 
-      width = {'10vw'} 
-      backgroundColor = {'yellow'} 
-      color = {'black'}
-      lh = {'0vw'}>
-      /
-      </Styledbutton>
-      <Styledbutton 
+      color = 'black'
+      backgroundColor = 'yellow'
+      pardisplay = 'inline-flex'>
+      /</Buttonv1>
+
+      <Buttonv1
       onck = {() => math('+')}
-      display = {'inline-flex'} 
-      pardisplay = {'inline-flex'} 
-      radius = {'0.3vw'} 
-      font = {'9vw'} 
-      margin = {'1vw 0vw 3vw 3vw'} 
-      height = {'10vw'} 
-      width = {'10vw'} 
-      backgroundColor = {'blue'} 
-      color = {'black'}
-      lh = {'0vw'}>
-      +
-      </Styledbutton>
-      <Styledbutton 
+      color = 'black'
+      backgroundColor = 'blue'
+      pardisplay = 'inline-flex'>
+      +</Buttonv1>     
+
+      <Buttonv1
       onck = {() => math('-')}
-      display = {'inline-flex'} 
-      pardisplay = {'inline-flex'} 
-      radius = {'0.3vw'} 
-      font = {'9vw'} 
-      margin = {'1vw 0vw 3vw 3vw'} 
-      height = {'10vw'} 
-      width = {'10vw'} 
-      backgroundColor = {'orange'} 
-      color = {'black'}
-      lh = {'0vw'}>
-      -
-      </Styledbutton>
+      color = 'black'
+      backgroundColor = 'orange'
+      pardisplay = 'inline-flex'>
+      -</Buttonv1>  
 
       <div style={{display:'inline-block', fontSize: '6vw', marginLeft: '5vw'}}>{count}</div>
       <div>
-      <Styledbutton 
+        
+      <Buttonv1
       onck = {() => pusher()}
-      display = {'inline-flex'} 
-      pardisplay = {'inline-flex'} 
-      radius = {'0.3vw'} 
-      font = {'3vw'} 
-      margin = {'1vw 0vw 3vw 3vw'} 
-      height = {'7vw'} 
-      width = {'23vw'} 
-      backgroundColor = {'orange'} 
-      color = {'black'}
-      lh = {'0vw'}>
-      save
-      </Styledbutton>
+      color = 'aqua'
+      backgroundColor = 'orange'
+      width = '24%'>
+      save</Buttonv1>  
+         
       </div>
       <div>{arr.map((item) =>(<div>{item}</div>))}</div>
         </div>
